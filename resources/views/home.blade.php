0a9d12f067c7f941251dc7321575c2e5b20f7749
Yuji Oda <yujioda1703@gmail.com>
2026-07-05 22:16:01 -0300

    Pasta de imagens e logo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <!-- conteúdo da home aqui -->
+            <h1 class="text-2xl font-bold mb-4">Bem-vindo ao Portal Etec! eu vou me matar</h1>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    if (! auth()->check()) {
+        return redirect()->route('login');
+    }
+
+    return view('home');
 });
 
 Route::get('/dashboard', function () {
