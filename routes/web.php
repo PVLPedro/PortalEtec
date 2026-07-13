abb17ba5704932f5b8e70f6c3e03abf80a5a6127
Yuji Oda <yujioda1703@gmail.com>
2026-07-12 21:51:14 -0300

    fix:'/'route

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Auth\PasswordController;
 
 Route::get('/', function () {
     if (!auth()->check()) {
-        return redirect()->route('register');
+        return redirect()->route('login');
     }
 
     return redirect()->route('dashboard');
